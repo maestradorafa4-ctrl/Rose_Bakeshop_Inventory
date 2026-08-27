bf993608199ad6560859c53375a83c029a673958
maestradorafa4-ctrl <maestradorafa4@gmail.com>
2026-08-27 17:18:18 +0800

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,1 +1,31 @@
+const SHEET_ID = "1-Fgf491BlXL_EQKguT3zwB9haNCnKxveD7ti6phHuYM";
+const SHEET_NAME = "Products";
 
+function doGet() {
+  return HtmlService.createTemplateFromFile("Index")
+    .evaluate()
+    .setTitle("Rose Bakeshop Inventory")
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function getSheet() {
+  const ss = SpreadsheetApp.openById(SHEET_ID);
+
+  let sheet = ss.getSheetByName(SHEET_NAME);
+
+  if (!sheet) {
+    sheet = ss.insertSheet(SHEET_NAME);
+
+    sheet.appendRow([
+      "ID",
+      "Product Name",
+      "Category",
+      "Quantity",
+      "Price",
+      "Stock Status",
+      "Date Added"
+    ]);
+  }
+
+  return sheet;
+}
